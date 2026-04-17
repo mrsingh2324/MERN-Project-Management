71763173c05998e631e474201eb37e7cf235afc6
Satyam Singh <satyam.12011172@gmail.com>
2026-04-17 15:13:59 +0530

    nginx config udpate

--- backend/server.js
+++ backend/server.js
@@ -25,5 +25,5 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: 'Something went wrong', error: err.message });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
